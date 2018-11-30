a6859652bcc7a16fb6256919e1f0f6b650f013f4
Christian Bedwell <32681621+ChristianBedwell@users.noreply.github.com>
2018-11-30 14:44:42 -0500

    Update Word.java

--- Word.java
+++ Word.java
@@ -1,45 +1,43 @@
 // URL: https://github.com/kinejohnsrud/naive-bayesian-spam-filter/tree/master/spam-filter/src/bayes
+import java.math.*;
 
 public class Word {
 	private String word;	// the word itself
 	private int spamCount;	// number of this words appearances in spam messages
 	private int hamCount;	// number of this words appearances in ham messages
-	private float spamRate;	// spamCount divided by total spam count
-	private float hamRate;	// hamCount divided by total ham count
+	private float probWordGivenSpam;	// spamCount divided by total spam count
+	private float probWordGivenHam;	// hamCount divided by total ham count
 	private float probOfSpam;	// probability of word being spam
 	
-	public Word(String word){
+	public Word(String word) {
 		this.word = word;
 		spamCount = 0;
 		hamCount = 0;
-		spamRate = 0.0f;
-		hamRate = 0.0f;
+		probWordGivenSpam = 0.0f;
+		probWordGivenHam = 0.0f;
 		probOfSpam = 0.0f;	
 	}
 	
-	public void countSpam(){
+	public void countSpam() {
 		spamCount++;
 	}
 	
-	public void countHam(){
+	public void countHam() {
 		hamCount++;
 	}
 	
-	// calculates the probability of spam, 
-	// and gives the smallest and biggest probabilities more precedence
-	public void calculateProbability(int totSpam, int totHam){
-		spamRate = spamCount / (float) totSpam;
-		hamRate = hamCount / (float) totHam;
-		
-		if(spamRate + hamRate > 0){
-			probOfSpam = spamRate / (spamRate + hamRate);
-		}
-		if(probOfSpam < 0.01f){
-			probOfSpam = 0.01f;
-		}
-		else if(probOfSpam > 0.99f){
-			probOfSpam = 0.99f;
-		}
+	// calculates the probability of spam, using Laplace smoothing
+	public float calculateWordSpamProbability(int totSpam, int numOfDistinctWords) {
+		// log(P(word[i]|spam)) 
+		probWordGivenSpam = (float) Math.log((spamCount + 1)/(totSpam + numOfDistinctWords));
+		return probWordGivenSpam;
+	}
+	
+	// calculates the probability of spam, using Laplace smoothing
+	public float calculateWordHamProbability(int totHam, int numOfDistinctWords) {
+		// log(P(word[i]|ham))
+		probWordGivenHam = (float) Math.log((hamCount + 1)/(totHam + numOfDistinctWords));
+		return probWordGivenHam;
 	}
 
 	public String getWord() {
@@ -47,15 +45,15 @@ public class Word {
 	}
 
 	public float getSpamRate() {
-		return spamRate;
+		return probWordGivenSpam;
 	}
 
 	public float getHamRate() {
-		return hamRate;
+		return probWordGivenHam;
 	}
 
-	public void setHamRate(float hamRate) {
-		this.hamRate = hamRate;
+	public void setHamRate(float probWordGivenHam) {
+		this.probWordGivenHam = probWordGivenHam;
 	}
 
 	public float getProbOfSpam() {
