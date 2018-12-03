3534229cb6bdc6e7a2ac2053d6ce8399c1115008
HBedwell24 <32681500+HBedwell24@users.noreply.github.com>
2018-12-03 14:41:51 -0500

    Delete Record.java

--- Record.java
+++
@@ -1,9 +0,0 @@
-public class Record {
-	double[] attributes;
-	int classLabel;
-	
-	Record(double[] attributes2, int classLabel2) {
-		this.attributes = attributes2;
-		this.classLabel = classLabel2;
-	}
-}
