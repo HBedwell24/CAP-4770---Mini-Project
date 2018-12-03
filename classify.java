f55c186451b16faddc6c912f8076c40cd43dba8e
Christian Bedwell <32681621+ChristianBedwell@users.noreply.github.com>
2018-12-03 18:19:53 -0500

    Update classify.java

--- classify.java
+++ classify.java
@@ -1,23 +1,33 @@
+// command line test driver for kNN and Naive Bayes
+
 public class classify {
 	
 	public static void main(String[] args) throws Exception { // main method
 		
 		// check input string for arguments and parse parameters
 		if(args[0].equals("knn") && args[1] != null && args[2] != null) {
-			
-			String charNum = args[1];
-			int k = Integer.parseInt(charNum);
-			String path = args[2];
-			kNN knn = new kNN(k, path);
-			knn.kNNaccuracy();
-		}
-	
+			// check for correct arguments (k and path)
+			if((args[1]).matches(".*\\d+.*")) {
+				String charNum = args[1];
+				int k = Integer.parseInt(charNum);
+				String path = args[2];
+				kNN knn = new kNN(k, path);
+				knn.kNNaccuracy();
+			}
+			else {
+				System.out.println("The k value provided as a parameter is not an integer!");
+			}
+		}	
 		else if (args[0].equals("NB") && args[1] != null) {
-			
+			// check for correct argument (path)
 			String path = args[1];
 			NaiveBayes naivebayes = new NaiveBayes(path);	
 			naivebayes.train();
 			naivebayes.test();
+		}
+		else {
+			System.out.println("Incorrect program input provided! Please re-run the program and follow the format: "
+					+ "'java classify knn' or 'java classify NB', including parameters for 'k' and/or 'relative file path' as necessary!");
 		}
 	}
 }
