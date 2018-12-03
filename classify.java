43e19dc815b5493228b23bf4185e0e205866b485
HBedwell24 <32681500+HBedwell24@users.noreply.github.com>
2018-12-03 17:30:16 -0500

    Update classify.java

--- classify.java
+++ classify.java
@@ -3,7 +3,7 @@ public class classify {
 	public static void main(String[] args) throws Exception { // main method
 		
 		// check input string for arguments and parse parameters
-		if(args[0] == "knn" && args[1] != null && args[2] != null) {
+		if(args[0].equals("knn") && args[1] != null && args[2] != null) {
 			
 			String charNum = args[1];
 			int k = Integer.parseInt(charNum);
@@ -12,13 +12,12 @@ public class classify {
 			knn.kNNaccuracy();
 		}
 	
-		else if (args[0] == "NB" && args[1] != null) {
+		else if (args[0].equals("NB") && args[1] != null) {
 			
 			String path = args[1];
-			NaiveBayes naivebayes = new NaiveBayes();	
-			int spamTotal = naivebayes.train(path + "/train.zip");
-			int spamCount = naivebayes.filter(path + "/test.zip");
-			naivebayes.accuracy(spamCount, spamTotal);
+			NaiveBayes naivebayes = new NaiveBayes(path);	
+			naivebayes.train();
+			naivebayes.test();
 		}
 	}
 }
